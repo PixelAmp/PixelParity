afdf68daccac7be64eede443c49b9604e935756a
Raul <reperez79@gmail.com>
2019-03-10 04:25:02 -0700

    Cleaning, Groundwork for comparison function
    Removed some modules that weren't being used
    Reorganized some functions
    Wrote out how file management should take place

--- PixelParity.py
+++ PixelParity.py
@@ -1,11 +1,12 @@
 import os, paramiko, sys, time, select, re
 from tkinter.filedialog import askdirectory
 from tkinter import messagebox
-from os import walk
-#from os.path import isfile, join
 
-#this program is basically a poor man's google drive that constantly update files on the remote server whenever a local change is made
-#A use case for this would be to write python on PC, but execute it on a raspberry pi
+#This program is basically a poor man's google drive that constantly update files on the remote server whenever a local change is made
+#use cases:
+#keep an offsite backup of a folder 
+#write python on PC, but execute it on a raspberry pi
+
 
 
 '''
@@ -14,6 +15,8 @@ DONE!   Select local folder
 DONE!   Select remote Folder
 DONE!   See data in local folder
 DONE!   See data in remote folder
+See if changes in local folder
+See if changes in remote folder
 
 Compare data in each folder
 Add ability to toggle whether to replace local file if remote one is newer
@@ -54,20 +57,22 @@ class ToolBox(object):
     channel=paramiko.SSHClient()
     #channel.open_sftp() #not initializing, once init SSH gets run it'll be here
     LocalDir=''
+    LocalStats={}
     RemoteDir=''
+    RemoteStats={}
     IgnoreFiles=[]
     IgnoreDir=['.git']
-    
 
-def getDir():
-    messagebox.showinfo('Select Directory', 'Please the folder that will be backed up')
-    WorkingDir = askdirectory()
-    print(WorkingDir)
-    return WorkingDir
+def GetFile(Rpath, Lpath):
+    ToolBox.sftp.get(Rpath,Lpath)
+    print("get")
 
+
+def SendFile(Rpath, Lpath):
+    ToolBox.sftp.put(Lpath, Rpath)
+    print("send")
 
 '''
-note to self:
 make this recoursive, have it return a dictionary of the values within that folder.
 If it finds a folder, call this function again and go intide that folder until it returns the values inside if itself
 
@@ -104,8 +109,8 @@ def getLocalStats(WorkingDir):
         else:
             print("Ignoring %s"%dir[num])
     
+    #ToolBox.LocalStats = localStats #not going to automatically update the master record, it should have to be updated once comparison is complete
     return localStats
-    
     
 def getRemoteStats(WorkingDir):
 
@@ -113,14 +118,12 @@ def getRemoteStats(WorkingDir):
     currdir=re.sub(r'.*/', '', WorkingDir) #gets only the current directory
     localStats.setdefault(currdir, [])
 
+    #get files within the current folder
     files = SSHCommand('ls %s -p | grep -v /'%WorkingDir, debug=False).split('\n')
-    #indnifiles = files.split('\n')
-    #print(indnifiles)
 
     #fills dictionay with files and their ages
     for num in range(len(files)):
         if files[num] not in ToolBox.IgnoreFiles and files[num]: #ignore Files
-            #AGE = SSHCommand("stat %s -c '%%Y'"%(files[num]),debug=True)
             localStats[currdir].append([files[num], int(SSHCommand("stat %s -c '%%Y'"%("'"+WorkingDir+'/'+files[num]+"'")).replace('\n',''))])
 
         else: 
@@ -128,21 +131,68 @@ def getRemoteStats(WorkingDir):
                 print("Ignoring %s"%repr(files[num]))
 
 
-    #get sub directories within this directory
-    #if there are no subdirectories an error gets sent out, so I catch it
+    #Get sub directories within this directory
+    #If there are no subdirectories an error gets sent out, so I effectively catch it
     dir = SSHCommand('ls -d %s/*/'%WorkingDir, debug=False, AcceptableError='No such file or directory').split('\n') 
     
     #recursively call this function to get the files within the subdirectory
     for num in range(len(dir)):
         if dir[num] not in ToolBox.IgnoreDir and dir[num]: #ignore Folders
-            localStats[currdir].append(getRemoteStats(dir[num][:-1]))
+            localStats[currdir].append(getRemoteStats(dir[num][:-1])) 
         else:
             if dir[num]:
                 print("Ignoring %s"%repr(dir[num]))
     
+    #ToolBox.RemoteStats = localStats #not going to automatically update the master record, it should have to be updated once comparison is complete
     return localStats
 
-def SSHCommand(command, debug=False, AcceptableError=''):
+'''
+5 possiblilitoes for deleting and creating new files:
+1 Always Delete if file is not found on one to keep parity between the two (though what is the difference between a new file vs a deleted one?)
+2 Never delete - always make sure file is at both places, so if it is missing on one, copy it over
+3 Local drive is law, remote drive is Slave - deletion and addition will only take place on the remote drive because the local drive is always right and remote drive is just a backup #this might be the one I am going to go with until #5 figured out
+4 Bias to remote drive - always copy follow whatever is on the remote drive - if new file copy it over , if file missing delete it
+5 If i knew about it previously and it is missing delete it on both, but if it is new make it exist on both (this requires a local copy of the layout of the folders) #this is the best method, but requires more work
+    this actually need to happen anyways during runtime 
+'''
+
+'''
+Keep a manafest of the directories in a test file that is stored in both folders, but ignore it during the comparison.
+Only update the file and copy it onto both after finishing a comparison, when it is totally up to date
+
+
+'''
+
+
+#comparison the first time this program is rerun to make sure manafests are correct and cache is set up
+def FirstTimeComparison():
+    
+    localStats = getLocalStats(ToolBox.LocalDir)
+    initSSH()
+    remotestats = getRemoteStats(ToolBox.RemoteDir)
+    
+    print('Local:') 
+    print(localStats)
+    print('Remote:')
+    print(remotestats)
+
+#compares the values of both directories and ballances them so they contain the same items
+def CompareDirectories():
+    ToolBox.LocalDir
+    ToolBox.RemoteDir
+
+
+#compares current local state to the cached local state to see if files have been added or deleted
+def CompareLocal():
+    return False
+
+#compares current remote state to the cached local state to see if files have been added or deleted
+def CompareRemote():
+    return False
+
+
+def SSHCommand(command, debug=False, AcceptableError='',Split=False):
+    
     error = ''
     output = ''
     if debug:
@@ -161,12 +211,12 @@ def SSHCommand(command, debug=False, AcceptableError=''):
         if stdout.channel.recv_ready():
             rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
             if len(rl) > 0:
-                output = stdout.channel.recv(4096).decode('utf-8')#.replace('\n','|')
+                output = stdout.channel.recv(4096).decode('utf-8')
     
-    
-    #print(repr(output))
-    return output
-
+    if Split: #returns each line as it's own value in an array
+        return output.split('\n')
+    else:
+        return output
 
 def initSSH():
     ToolBox.channel = paramiko.SSHClient()
@@ -202,47 +252,30 @@ def GetSSH():
             if hold.lower() != 'n':
                 sys.exit()
 
+def getLocalDir():
+    messagebox.showinfo('Select Directory', 'Please select the folder that will be backed up')
+    WorkingDir = askdirectory()
+    print(WorkingDir)
+    return WorkingDir
 
-LocalDir=''
-RemoteDir=''
-def GetFile(Rpath, Lpath):
-    ToolBox.sftp.get(Rpath,Lpath)
-    print("get")
-
-
-def SendFile(Rpath, Lpath):
-    ToolBox.sftp.put(Lpath, Rpath)
-    print("send")
-
-def main():
-    
-    #LocalDir = getDir()
-    LocalDir = 'C:/Users/Pixel Amp/Desktop/PixelParity'
-    RemoteDir = '/home/pi/PixelParity'
-
-    
-    localStats = getLocalStats(LocalDir)
-    
-
-    initSSH()
-    remotestats = getRemoteStats(RemoteDir)
-    
-    print('Local:') 
-    print(localStats)
-    print('Remote:')
-    print(remotestats)
-
-    '''
-    initSSH()
-    #getRemoteStats()
-    
+#nned to think of a better name for this cunction, but it basically lets the user send in commands to the SSH deivce
+def EmulateConsole():    
     command=''
     print('Enter SSH Command:')
     while command != 'exit':
         command = input()
-        response = SSHCommand(command)#, expect=':~$')
+        response = SSHCommand(command)
         print(response)
-    '''
+
+
+def main():
+    initSSH()
+    #LocalDir = getDir()
+    ToolBox.LocalDir = 'C:/Users/Pixel Amp/Desktop/PixelParity'
+    ToolBox.RemoteDir = '/home/pi/PixelParity'
+
+    FirstTimeComparison()
+
     ToolBox.channel.close()
 
 
